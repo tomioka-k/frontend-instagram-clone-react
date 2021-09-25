9455e47a972f00900f2b419e0d82e13611e78a24
tomioka-k <tomioka-k@tajima.co.jp>
2021-09-26 01:30:27 +0900

    create fetch api

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -5,6 +5,75 @@ import { PROPS_AUTHEN, PROPS_PROFILE, NICK_NAME } from "../types";
 
 const apiUrl = process.env.REACT_APP_DEV_API_URL;
 
+export const fetchAsyncLogin = createAsyncThunk(
+	"auth/post",
+	async (authen: PROPS_AUTHEN) => {
+		const res = await axios.post(`${apiUrl}authen/jwt/create/`, authen, {
+			headers: {
+				"Content-Type": "Application/json",
+			},
+		});
+		return res.data;
+	}
+);
+
+export const fetchAsyncRegister = createAsyncThunk(
+	"auth/register",
+	async (authen: PROPS_AUTHEN) => {
+		const res = await axios.post(`${apiUrl}api/register/`, authen, {
+			headers: {
+				"Content-Type": "Application/json",
+			},
+		});
+		return res.data;
+	}
+);
+
+export const fetchAsyncCreateProfile = createAsyncThunk(
+	"profile/post",
+	async (nickName: PROPS_PROFILE) => {
+		const res = await axios.post(`${apiUrl}api/profile`, nickName, {
+			headers: {
+				"Content-Type": "Application/json",
+				Authorization: `JWT ${localStorage.localJWT}`,
+			},
+		});
+		return res.data;
+	}
+);
+
+export const fetchAsyncUpdateProf = createAsyncThunk(
+	"profile/put",
+	async (profile: PROPS_PROFILE) => {
+		const uploadData = new FormData();
+		uploadData.append("nickName", profile.nickName);
+		profile.img && uploadData.append("img", profile.img, profile.img.name);
+		const res = await axios.put(
+			`${apiUrl}api/profile/${profile.id}`,
+			uploadData,
+			{
+				headers: {
+					"Content-Type": "Application/json",
+					Authorization: `JWT ${localStorage.localJWT}`,
+				},
+			}
+		);
+		return res.data;
+	}
+);
+
+export const fetchAsyncGetMyProf = createAsyncThunk(
+	"profile/myprofile",
+	async () => {
+		const res = await axios.get(`${apiUrl}api/myprofile/`, {
+			headers: {
+				Authorization: `JWT ${localStorage.localJWT}`,
+			},
+		});
+		return res.data[0];
+	}
+);
+
 const initialState = {
 	openSignIn: true,
 	openSignUp: false,
