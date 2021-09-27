54eeaa7b978aeb91e13cb00b6bb9c0f529c136a1
tomioka-k <tomioka-k@tajima.co.jp>
2021-09-27 23:37:40 +0900

    post component

--- src/features/post/Post.tsx
+++ src/features/post/Post.tsx
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import styles from "./Post.module.css";
+
 import { makeStyles } from "@material-ui/core/styles";
 import { Avatar, Divider, Checkbox } from "@material-ui/core";
-import { Favorite, FavoriteBorder, Height } from "@material-ui/icons";
+import { Favorite, FavoriteBorder } from "@material-ui/icons";
 
-import { AvatarGroup } from "@material-ui/lab";
-import { useDispatch, useSelector } from "react-redux";
+import AvatarGroup from "@material-ui/lab/AvatarGroup";
+
+import { useSelector, useDispatch } from "react-redux";
 import { AppDispatch } from "../../app/store";
 
 import { selectProfiles } from "../auth/authSlice";
+
 import {
-	fetchAsyncPatchLiked,
-	fetchAsyncPostComment,
-	fetchPostEnd,
-	fetchPostStart,
 	selectComments,
+	fetchPostStart,
+	fetchPostEnd,
+	fetchAsyncPostComment,
+	fetchAsyncPatchLiked,
 } from "./postSlice";
 
 import { PROPS_POST } from "../types";
@@ -70,7 +73,82 @@ const Post: React.FC<PROPS_POST> = ({
 		await dispatch(fetchPostEnd());
 	};
 
-	return <div></div>;
+	if (title) {
+		return (
+			<div className={styles.post}>
+				<div className={styles.post_header}>
+					<Avatar className={styles.post_avatar} src={prof[0]?.img} />
+					<h3>{prof[0]?.nickName}</h3>
+				</div>
+				<img className={styles.post_image} src={imageUrl} alt="" />
+
+				<h4 className={styles.post_text}>
+					<Checkbox
+						className={styles.post_checkBox}
+						icon={<FavoriteBorder />}
+						checkedIcon={<Favorite />}
+						checked={liked.some((like) => like === loginId)}
+						onChange={handlerLiked}
+					/>
+					<strong> {prof[0]?.nickName}</strong> {title}
+					<AvatarGroup max={7}>
+						{liked.map((like) => (
+							<Avatar
+								className={styles.post_avatarGroup}
+								key={like}
+								src={profiles.find((prof) => prof.userProfile === like)?.img}
+							/>
+						))}
+					</AvatarGroup>
+				</h4>
+
+				<Divider />
+				<div className={styles.post_comments}>
+					{commentsOnPost.map((comment) => (
+						<div key={comment.id} className={styles.post_comment}>
+							<Avatar
+								src={
+									profiles.find(
+										(prof) => prof.userProfile === comment.userComment
+									)?.img
+								}
+								className={classes.small}
+							/>
+							<p>
+								<strong className={styles.post_strong}>
+									{
+										profiles.find(
+											(prof) => prof.userProfile === comment.userComment
+										)?.nickName
+									}
+								</strong>
+								{comment.text}
+							</p>
+						</div>
+					))}
+				</div>
+
+				<form className={styles.post_commentBox}>
+					<input
+						className={styles.post_input}
+						type="text"
+						placeholder="add a comment"
+						value={text}
+						onChange={(e) => setText(e.target.value)}
+					/>
+					<button
+						disabled={!text.length}
+						className={styles.post_button}
+						type="submit"
+						onClick={postComment}
+					>
+						Post
+					</button>
+				</form>
+			</div>
+		);
+	}
+	return null;
 };
 
 export default Post;
